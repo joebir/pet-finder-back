dcd0b31430fc64273dbd46b87fbb0795a1665963
HELEN_COX <HELEN.COX2969@GMAIL.COM>
2020-11-18 19:19:01 -0500

    seeding pets

---
+++ seeding_pets.py
@@ -0,0 +1,36 @@
+
+{
+    "petName": "Lucy",
+    "aboutPet": "cat, British Shothair, yellow eyes, grey",
+    "dateLost": "12/09/2019",
+    "found": True,
+    "owner": "Tim Krammer",
+    "photo": "https://www.petful.com/wp-content/uploads/2016/07/british-shorthair-750x419.jpg"
+},
+
+{
+    "petName": "Buddy",
+    "aboutPet": "dog, Labrador, golden color",
+    "dateLost": "10/04/2020",
+    "found": False,
+    "owner": "Michael Johnson",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Labrador_on_Quantock_%282175262184%29.jpg/220px-Labrador_on_Quantock_%282175262184%29.jpg"
+},
+
+{
+    "petName": "Oliver",
+    "aboutPet": "dog, Labrador, golden color",
+    "dateLost": "09/21/2020",
+    "found": False,
+    "owner": "Takisha Williams",
+    "photo": "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20122208/Samoyed-standing-in-the-forest.jpg"
+},
+
+{
+    "petName": "Molly",
+    "aboutPet": "cat, went away and never came back, white fluffy fur, green eyes",
+    "dateLost": "04/24/2020",
+    "found": False,
+    "owner": "Sheila Thompson",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/1/15/White_Persian_Cat.jpg"
+},
---
+++ seeding_users.py
@@ -0,0 +1,36 @@
+
+
+{
+    "username": "Jim",
+    "email": "Jimmyhaff3@gmail.com",
+    "password": "1234",
+    "phone": "909-454-4455"
+},
+
+{
+    "username": "Tim",
+    "email": "TimNickerson@hotmail.com",
+    "password": "M_12345",
+    "phone": "404-454-3322"
+},
+
+{
+    "username": "Jannette Smith",
+    "email": "JeannetteSmith_3@mail.com",
+    "password": "New#3322",
+    "phone": "770-995-4433"
+},
+
+{
+    "username": "Rosalie French",
+    "email": "RFrench24@yahoo.com",
+    "password": "you&3443",
+    "phone": "478-885-4334"
+},
+
+{
+    "username": "Billy Anderson",
+    "email": "billy-44@gmail.com",
+    "password": "7667",
+    "phone": "212-655-1188"
+},
